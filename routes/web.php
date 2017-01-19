d128a09770dff15f5e9391a4cc5edd9d83940bc2
Efriandika Pratama <efriandika.pratama@bni.co.id>
2017-01-19 08:51:38 +0700

    Preparing forgot password feature

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,10 @@ Route::group(['prefix' => 'portal', 'middleware' => 'web'], function () {
     Route::get('/login/register', 'Portal\Auth\LoginController@showRegistrationForm')->name('portal-register');
     Route::post('/login/register', 'Portal\Auth\LoginController@register');
     Route::get('/logout', 'Portal\Auth\LoginController@logout')->name('portal-logout');
+    Route::get('/password/reset', 'Portal\Auth\LoginController@logout')->name('portal-forgot-password');
+    Route::post('/password/reset', 'Portal\Auth\LoginController@register');
+    Route::get('/password/login-token', 'Portal\Auth\LoginController@logout')->name('portal-login-token');
+    Route::post('/password/login-token', 'Portal\Auth\LoginController@register');
 });
 
 /*
