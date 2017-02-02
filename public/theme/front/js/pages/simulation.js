fce78211384a717e8e308d00d8c5f36488221554
Romy Adzani A <Romy.Adzani@bni.co.id>
2017-02-02 16:19:39 +0700

    #simulation update

--- public/theme/front/js/pages/simulation.js
+++ public/theme/front/js/pages/simulation.js
@@ -1,3 +1,5 @@
+//Author: Romy Adzani A
+
 $('input.currency').numeric().keydown(function() {
     $(this).val($(this).val().replace(/\./g, ''));
 }).keyup(function() {
@@ -34,70 +36,64 @@ $('input[name="billingRadio"]').click(function() {
 $('[data-toggle="tooltip"]').tooltip();
 
 // Simulation Chart
-var ctx = $('canvas#simulation');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["40", "45", "50", "55", "60"],
-        datasets: [
-            {
-                label: "Dana Awal",
-                backgroundColor: "rgba(49,133,156,.2)",
-                borderColor: "rgba(49,133,156,1)",
-                pointBackgroundColor: "rgba(49,133,156,1)",
-                pointBorderColor: "#fff",
-                pointHoverBackgroundColor: "#fff",
-                pointHoverBorderColor: "rgba(49,133,156,1)",
-                data: [0, 0, 0, 0, 0]
+var ctx = $('canvas#simulation'),
+    
+    //Currency format will be applied for getting result only
+    getChartCurrencyFormat = function(isgetresult) {
+        if (isgetresult) {
+            return [{ ticks: { beginAtZero: true, callback: function(value) {
+                return 'Rp' + ((parseInt(value) >= 1000) ? value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") : value);
+            } } }];
+        } else {
+            return false;
+        }
+    },
+
+    //Get dataset style and dataset itself
+    getDatasets = function (label, backgroundColor, borderColor, pointBackgroundColor, pointHoverBorderColor, data) {
+        return { label: label, backgroundColor: backgroundColor, borderColor: borderColor, pointBackgroundColor: pointBackgroundColor, pointBorderColor: "#fff", pointHoverBackgroundColor: "#fff", pointHoverBorderColor: pointHoverBorderColor, data: data }
+    },
+
+    //Create chart JSON
+    getChart = function(a, isgetresult) {
+        return {
+            type: 'line',
+            data: {
+                labels: a[0],
+                datasets: [
+                    getDatasets('Dana Awal', 'rgba(49,133,156,.2)', 'rgba(49,133,156,1)', 'rgba(49,133,156,1)', 'rgba(49,133,156,1)', a[1]),
+                    getDatasets('Iuran', 'rgba(192,80,77,.2)', 'rgba(192,80,77,1)', 'rgba(192,80,77,1)', 'rgba(192,80,77,1)', a[2]),
+                    getDatasets('Pengembangan', 'rgba(119,147,60,.2)', 'rgba(119,147,60,1)', 'rgba(119,147,60,1)', 'rgba(119,147,60,1)', a[3]),
+                    getDatasets('Saldo Akhir', 'rgba(179,181,198,.2)', 'rgba(179,181,198,1)', 'rgba(179,181,198,1)', 'rgba(179,181,198,1)', a[4])
+                ]
             },
-            {
-                label: "Iuran",
-                backgroundColor: "rgba(192,80,77,.2)",
-                borderColor: "rgba(192,80,77,1)",
-                pointBackgroundColor: "rgba(192,80,77,1)",
-                pointBorderColor: "#fff",
-                pointHoverBackgroundColor: "#fff",
-                pointHoverBorderColor: "rgba(192,80,77,1)",
-                data: [0, 0, 0, 0, 0]
-            },
-            {
-                label: "Pengembangan",
-                backgroundColor: "rgba(119,147,60,.2)",
-                borderColor: "rgba(119,147,60,1)",
-                pointBackgroundColor: "rgba(119,147,60,1)",
-                pointBorderColor: "#fff",
-                pointHoverBackgroundColor: "#fff",
-                pointHoverBorderColor: "rgba(119,147,60,1)",
-                data: [0, 0, 0, 0, 0]
-            },
-            {
-                label: "Saldo Akhir",
-                backgroundColor: "rgba(179,181,198,.2)",
-                borderColor: "rgba(179,181,198,1)",
-                pointBackgroundColor: "rgba(179,181,198,1)",
-                pointBorderColor: "#fff",
-                pointHoverBackgroundColor: "#fff",
-                pointHoverBorderColor: "rgba(179,181,198,1)",
-                data: [0, 0, 0, 0, 0]
-            }
-        ]
-    }
-});
+            options: { scales: { xAxes: [{ scaleLabel: { display: true, labelString: 'Usia (dalam tahun)' } }], yAxes: getChartCurrencyFormat(isgetresult) } }
+        }
+    },
+
+    //Initialize new chart
+    myChart = new Chart(ctx, getChart([ [40, 45, 50, 55, 60], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0] ], false));
 
 $('.calculate').click(function() {
-    var datecontrol = $('.date-control > *');
-    var checked = true;
+    var datecontrol = $('.date-control > *'),
+        checked = true; //for future update of consumer record
+    
+    //for future update of consumer record (simulation can be used when birth date of consumer is fully set)
     datecontrol.children(':selected').each(function() {
         checked = checked && ($(this).index() > 0);
     });
+
     if (checked) {
+        
+        //Lines below are for future update of consumer record
         /*var dob = new Date((datecontrol.eq(2).val()) + '-' + (datecontrol.eq(1).children(':selected').index()) + '-' + (datecontrol.eq(0).children(':selected').index()));
         var today = new Date();
         var age = Math.floor((today-dob) / (365.25 * 24 * 60 * 60 * 1000));*/
+
         var age = parseInt($('#age option:selected').attr('data-value')),
             retirementage = parseInt($('#retirement-age option:selected').attr('data-value')),
             durationmonth = (retirementage - age) * 12,
-            datachart = [ [], [], [], [], [] ]; //5 years behind of retirement age
+            datachart = [ [], [], [], [], [] ]; //each arrays contains 25 to 5 years before retirement age of data
             countlast = durationmonth / 60;
             countlast = (countlast > 4) ? 4 : (countlast - 1);
             isannualstartingbalance = $('input[name="topupRadio"]:checked').parent().is(':last-child'),
@@ -134,76 +130,19 @@ $('.calculate').click(function() {
                 }
             }
         }
-        $('#total-funding span').text('Rp').siblings('b').text(parseInt(accumulatedstartingbalance + accumulatedbilling).toString().replace(/(?!^)(?=(?:\d{3})+(?:\.|$))/gm, '.'));
-        $('#total-development span').text('Rp').siblings('b').text(parseInt(datachart[3][datachart[3].length - 1]).toString().replace(/(?!^)(?=(?:\d{3})+(?:\.|$))/gm, '.'));
-        $('#total-fund span').text('Rp').siblings('b').text(parseInt(accumulatedfund).toString().replace(/(?!^)(?=(?:\d{3})+(?:\.|$))/gm, '.'));
+        var chartAnimateNumber = function(a) {
+            return {
+                number: a,
+                numberStep: $.animateNumber.numberStepFactories.separator('.'),
+            }
+        };
+        $('#total-funding span').text('Rp').siblings('b').animateNumber(chartAnimateNumber(parseInt(accumulatedstartingbalance + accumulatedbilling)), 1500);
+        $('#total-development span').text('Rp').siblings('b').animateNumber(chartAnimateNumber(parseInt(datachart[3][datachart[3].length - 1])), 1500);
+        $('#total-fund span').text('Rp').siblings('b').animateNumber(chartAnimateNumber(parseInt(accumulatedfund)), 1500);
         $('iframe.chartjs-hidden-iframe').remove();
         ctx.after('<canvas id="simulation" class="col-xs-12" height="250"></canvas>').remove();
         ctx = $('canvas#simulation');
-        myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: datachart[0],
-                datasets: [
-                    {
-                        label: "Dana Awal",
-                        backgroundColor: "rgba(49,133,156,.2)",
-                        borderColor: "rgba(49,133,156,1)",
-                        pointBackgroundColor: "rgba(49,133,156,1)",
-                        pointBorderColor: "#fff",
-                        pointHoverBackgroundColor: "#fff",
-                        pointHoverBorderColor: "rgba(49,133,156,1)",
-                        data: datachart[1]
-                    },
-                    {
-                        label: "Iuran",
-                        backgroundColor: "rgba(192,80,77,.2)",
-                        borderColor: "rgba(192,80,77,1)",
-                        pointBackgroundColor: "rgba(192,80,77,1)",
-                        pointBorderColor: "#fff",
-                        pointHoverBackgroundColor: "#fff",
-                        pointHoverBorderColor: "rgba(192,80,77,1)",
-                        data: datachart[2]
-                    },
-                    {
-                        label: "Pengembangan",
-                        backgroundColor: "rgba(119,147,60,.2)",
-                        borderColor: "rgba(119,147,60,1)",
-                        pointBackgroundColor: "rgba(119,147,60,1)",
-                        pointBorderColor: "#fff",
-                        pointHoverBackgroundColor: "#fff",
-                        pointHoverBorderColor: "rgba(119,147,60,1)",
-                        data: datachart[3]
-                    },
-                    {
-                        label: "Saldo Akhir",
-                        backgroundColor: "rgba(179,181,198,.2)",
-                        borderColor: "rgba(179,181,198,1)",
-                        pointBackgroundColor: "rgba(179,181,198,1)",
-                        pointBorderColor: "#fff",
-                        pointHoverBackgroundColor: "#fff",
-                        pointHoverBorderColor: "rgba(179,181,198,1)",
-                        data: datachart[4]
-                    }
-                ]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true,
-                            callback: function(value) {
-                                if(parseInt(value) >= 1000){
-                                    return 'Rp' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-                                } else {
-                                    return 'Rp' + value;
-                                }
-                            }
-                        }
-                    }]
-                }
-            }
-        });
+        myChart = new Chart(ctx, getChart(datachart, true));
     } else {
         console.log('Mohon isi lengkap');
     }
