1ab57e59ff0536d86c85581ad8d64f119c057a32
Efriandika Pratama <efriandika.pratama@bni.co.id>
2017-02-14 13:50:51 +0700

    Updated assets

---
+++ public/theme/front/js/simponi.js
@@ -0,0 +1,38 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Offset for Main Navigation
+    var navOffsetTop = 150;
+    $('#main-nav').affix({
+        offset: {
+            top: navOffsetTop
+        }
+    });
+
+    // Initialize and Configure Scroll Reveal Animation
+    window.sr = ScrollReveal();
+    sr.reveal('.sr-icons', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 200);
+    sr.reveal('.sr-button', {
+        duration: 1000,
+        delay: 200
+    });
+    sr.reveal('.sr-contact', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 300);
+
+})(jQuery); // End of use strict
