841af13a38d34818f8bd9ed833481450ea652214
Efriandika Pratama <efriandika.pratama@bni.co.id>
2017-03-03 16:51:10 +0700

    Added account balance

--- resources/views/portal/dashboard/index.blade.php
+++ resources/views/portal/dashboard/index.blade.php
@@ -3,7 +3,46 @@
 @section('portalContent')
     <h3>Inquiry Saldo</h3>
 
-    Under Construction
+    <strong>Kategori Akun:</strong> {{ $user->accountPerson->accountCategory }}
+
+    <section class="section-box inquiry-box">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="section-box-item">
+                    <div class="box-header">
+                        Total Setoran Anda
+                    </div>
+                    <div class="box-body">
+                        {{ idr($user->accountPerson->totAmount) }}
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="section-box-item">
+                    <div class="box-header">
+                        Total Pengembagan Dana
+                    </div>
+                    <div class="box-body">
+                        {{ idr($user->accountPerson->totBunga) }}
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="section-box-item">
+                    <div class="box-header">
+                        Total Dana BNI Simponi Anda
+                    </div>
+                    <div class="box-body">
+                        {{ idr($user->accountPerson->balance) }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <p class="help-block">
+        Saldo per tanggal: {{ date('d-m-Y H:i') }}
+    </p>
 @endsection
 
 @push('portalFootScript')
