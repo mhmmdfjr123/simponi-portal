f929ad7649ca9cd4bbe577139af4b503ef420600
Efriandika Pratama <efriandika.pratama@bni.co.id>
2017-03-14 20:24:27 +0700

    Disable un-used feature

--- resources/views/layouts/backoffice.blade.php
+++ resources/views/layouts/backoffice.blade.php
@@ -50,7 +50,7 @@
                 <div><strong>{{ Auth::user()->name }}</strong></div>
             </div>
             <div class="btn-group" style="margin-top: 4px;">
-                <a href="javascript:void(0)" onclick="alertPopUp('Pusat Bantuan', 'Butuh bantuan? <br /> hubungi <strong>efriandika@gmail.com</strong>', 'Tutup')" title="Bantuan" class="btn btn-xs btn-primary btn-outline dark"><i class="fa fa-support"></i></a>
+                {{--<a href="javascript:void(0)" onclick="alertPopUp('Pusat Bantuan', 'Butuh bantuan? <br /> hubungi <strong>efriandika@gmail.com</strong>', 'Tutup')" title="Bantuan" class="btn btn-xs btn-primary btn-outline dark"><i class="fa fa-support"></i></a>--}}
                 <a href="{{ url('backoffice/profile/edit') }}" title="Edit Profile" class="btn btn-xs btn-primary btn-outline dark"><i class="fa fa-user"></i></a>
                 <a href="javascript:void(0)" onclick="confirmDirectPopUp('{{ url('logout') }}', 'Konfirmasi Logout', 'Apakah anda yakin ingin keluar?', 'Ya', 'Tidak');" class="btn btn-xs btn-danger btn-outline dark"><i class="fa fa-power-off"></i></a>
             </div>
@@ -134,123 +134,6 @@
         </ul>
 
         <ul class="nav navbar-nav navbar-right">
-            <li class="dropdown">
-                <a href="#notifications" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                    <i class="px-navbar-icon fa fa-bullhorn"></i>
-                    <span class="px-navbar-icon-label">Notifications</span>
-                    <span class="px-navbar-label label label-success">5</span>
-                </a>
-                <div class="dropdown-menu p-a-0" style="width: 300px">
-                    <div id="navbar-notifications" style="height: 280px; position: relative;">
-                        <div class="widget-notifications-item">
-                            <div class="widget-notifications-title text-danger">SYSTEM</div>
-                            <div class="widget-notifications-description"><strong>Error 500</strong>: Syntax error in index.php at line <strong>461</strong>.</div>
-                            <div class="widget-notifications-date">12h ago</div>
-                            <div class="widget-notifications-icon fa fa-hdd-o bg-danger"></div>
-                        </div>
-
-                        <div class="widget-notifications-item">
-                            <div class="widget-notifications-title text-info">STORE</div>
-                            <div class="widget-notifications-description">You have <strong>9</strong> new orders.</div>
-                            <div class="widget-notifications-date">12h ago</div>
-                            <div class="widget-notifications-icon fa fa-truck bg-info"></div>
-                        </div>
-
-                        <div class="widget-notifications-item">
-                            <div class="widget-notifications-title text-default">CRON DAEMON</div>
-                            <div class="widget-notifications-description">Job <strong>"Clean DB"</strong> has been completed.</div>
-                            <div class="widget-notifications-date">12h ago</div>
-                            <div class="widget-notifications-icon fa fa-clock-o bg-default"></div>
-                        </div>
-
-                        <div class="widget-notifications-item">
-                            <div class="widget-notifications-title text-success">SYSTEM</div>
-                            <div class="widget-notifications-description">Server <strong>up</strong>.</div>
-                            <div class="widget-notifications-date">12h ago</div>
-                            <div class="widget-notifications-icon fa fa-hdd-o bg-success"></div>
-                        </div>
-
-                        <div class="widget-notifications-item">
-                            <div class="widget-notifications-title text-warning">SYSTEM</div>
-                            <div class="widget-notifications-description"><strong>Warning</strong>: Processor load <strong>92%</strong>.</div>
-                            <div class="widget-notifications-date">12h ago</div>
-                            <div class="widget-notifications-icon fa fa-hdd-o bg-warning"></div>
-                        </div>
-                    </div>
-
-                    <a href="#" class="widget-more-link">MORE NOTIFICATIONS</a>
-                </div>
-            </li>
-
-            <li class="dropdown">
-                <a href="https://google.com" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                    <i class="px-navbar-icon fa fa-envelope"></i>
-                    <span class="px-navbar-icon-label">Income messages</span>
-                    <span class="px-navbar-label label label-danger">8</span>
-                </a>
-                <div class="dropdown-menu p-a-0" style="width: 300px;">
-                    <div id="navbar-messages" style="height: 280px; position: relative;">
-                        <div class="widget-messages-alt-item">
-                            <img src="{{ asset('theme/backoffice/demo/avatars/2.jpg') }}" alt="" class="widget-messages-alt-avatar">
-                            <a href="#" class="widget-messages-alt-subject text-truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</a>
-                            <div class="widget-messages-alt-description">from <a href="#">Robert Jang</a></div>
-                            <div class="widget-messages-alt-date">2h ago</div>
-                        </div>
-
-                        <div class="widget-messages-alt-item">
-                            <img src="{{ asset('theme/backoffice/demo/avatars/3.jpg') }}" alt="" class="widget-messages-alt-avatar">
-                            <a href="#" class="widget-messages-alt-subject text-truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</a>
-                            <div class="widget-messages-alt-description">from <a href="#">Michelle Bortz</a></div>
-                            <div class="widget-messages-alt-date">2h ago</div>
-                        </div>
-
-                        <div class="widget-messages-alt-item">
-                            <img src="{{ asset('theme/backoffice/demo/avatars/4.jpg') }}" alt="" class="widget-messages-alt-avatar">
-                            <a href="#" class="widget-messages-alt-subject text-truncate">Lorem ipsum dolor sit amet.</a>
-                            <div class="widget-messages-alt-description">from <a href="#">Timothy Owens</a></div>
-                            <div class="widget-messages-alt-date">2h ago</div>
-                        </div>
-
-                        <div class="widget-messages-alt-item">
-                            <img src="{{ asset('theme/backoffice/demo/avatars/5.jpg') }}" alt="" class="widget-messages-alt-avatar">
-                            <a href="#" class="widget-messages-alt-subject text-truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</a>
-                            <div class="widget-messages-alt-description">from <a href="#">Denise Steiner</a></div>
-                            <div class="widget-messages-alt-date">2h ago</div>
-                        </div>
-
-                        <div class="widget-messages-alt-item">
-                            <img src="{{ asset('theme/backoffice/demo/avatars/2.jpg') }}" alt="" class="widget-messages-alt-avatar">
-                            <a href="#" class="widget-messages-alt-subject text-truncate">Lorem ipsum dolor sit amet.</a>
-                            <div class="widget-messages-alt-description">from <a href="#">Robert Jang</a></div>
-                            <div class="widget-messages-alt-date">2h ago</div>
-                        </div>
-
-                        <div class="widget-messages-alt-item">
-                            <img src="{{ asset('theme/backoffice/demo/avatars/3.jpg') }}" alt="" class="widget-messages-alt-avatar">
-                            <a href="#" class="widget-messages-alt-subject text-truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</a>
-                            <div class="widget-messages-alt-description">from <a href="#">Michelle Bortz</a></div>
-                            <div class="widget-messages-alt-date">2h ago</div>
-                        </div>
-
-                        <div class="widget-messages-alt-item">
-                            <img src="{{ asset('theme/backoffice/demo/avatars/4.jpg') }}" alt="" class="widget-messages-alt-avatar">
-                            <a href="#" class="widget-messages-alt-subject text-truncate">Lorem ipsum dolor sit amet.</a>
-                            <div class="widget-messages-alt-description">from <a href="#">Timothy Owens</a></div>
-                            <div class="widget-messages-alt-date">2h ago</div>
-                        </div>
-
-                        <div class="widget-messages-alt-item">
-                            <img src="{{ asset('theme/backoffice/demo/avatars/5.jpg') }}" alt="" class="widget-messages-alt-avatar">
-                            <a href="#" class="widget-messages-alt-subject text-truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</a>
-                            <div class="widget-messages-alt-description">from <a href="#">Denise Steiner</a></div>
-                            <div class="widget-messages-alt-date">2h ago</div>
-                        </div>
-                    </div>
-
-                    <a href="#" class="widget-more-link">MORE MESSAGES</a>
-                </div> <!-- / .dropdown-menu -->
-            </li>
-
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                     <img data-name="{{ Auth::user()->name }}" class="img-profile-name px-navbar-image" />
