1b54bd2c55ced0047bb926ccd241e4f499bd25ad
Efriandika Pratama <efriandika.pratama@bni.co.id>
2017-03-24 09:07:17 +0700

    Added post single

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -2,8 +2,14 @@
 
 @section('content')
     <div class="content-header">
-        <h1>{!! $post->title !!}</h1>
+        <h1 style="max-width: 700px; margin-left: auto; margin-right: auto">{!! $post->title !!}</h1>
         <hr class="title">
+        <div style="text-align: center; font-size: 12px; margin-top: -10px;">
+            Di posting pada: {{ $post->created_at->format('d-m-Y H:i:s') }} oleh {{ $post->user->name }}
+        </div>
+        <div style="margin-bottom: 30px; text-align: center; font-size: 13px">
+            {!! implode(', ', $categories) !!}
+        </div>
     </div>
 
     <div class="container slim-content-container">
